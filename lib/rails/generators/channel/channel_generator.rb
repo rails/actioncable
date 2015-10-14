eff96f3570ed69b595f1c3ce1cfc7cbd7882e998
Austin Seven Horning <austinseven@outlook.com>
2015-10-14 17:53:26 -0400

    Added Generators for Actioncable
    Added Generators for Actioncable

---
+++ lib/rails/generators/channel/channel_generator.rb
@@ -0,0 +1,42 @@
+require 'rails/generators/named_base'
+
+module Rails
+  module Generators
+    class ChannelGenerator < Rails::Generators::NamedBase
+      desc 'This generator creates a cable channel file in app/channels as well as the coffeescript file in app/assets/javascripts/channels'
+      source_root File.expand_path('../templates', __FILE__)
+      check_class_collision suffix: 'Channel'
+
+      hook_for :test_framework
+
+      def create_channel_files
+        initialize_actioncable_requirements
+        template 'channel.rb.erb', File.join('app/channels', class_path, "#{file_name}_channel.rb")
+        template 'channel.coffee.erb', File.join('app/assets/javascripts/channels', "#{file_name}_channel.coffee")
+      end
+
+      def initialize_actioncable_requirements
+        unless File.exist?('app/channels/application_cable/channel.rb')
+          template 'application_cable_channel.rb.erb', File.join('app/channels/application_cable', class_path, "channel.rb")
+        end
+        unless File.exist?('app/channels/application_cable/connection.rb')
+          template 'application_cable_connection.rb.erb', File.join('app/channels/application_cable', class_path, "connection.rb")
+        end
+        unless File.exist?('app/assets/javascripts/channels')
+          template 'index.coffee.erb', File.join('app/assets/javascripts/channels', "index.coffee")
+        end
+        unless File.read("app/assets/javascripts/application.js").include?("//= require channels")
+          inject_into_file "app/assets/javascripts/application.js", "//= require channels", :before => "//= require_tree .\n"
+        end
+      end
+
+      def channel_name
+        class_name.camelize(:lower)
+      end
+
+      def channel_class
+        "#{class_name}Channel"
+      end
+    end
+  end
+end
---
+++ lib/rails/generators/channel/templates/application_cable_connection.rb.erb
@@ -0,0 +1,19 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.name
+    end
+
+    protected
+      def find_verified_user
+        if verified_user = User.find_by(id: cookies.signed[:user_id])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
+  end
+end
